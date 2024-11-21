6a13d6dd99619169b9d7d64e866406cc97ef200c
Lai Jia Hong <henry@Lais-MacBook-Air.local>
2024-11-21 23:32:10 +0800

    Emotion Recognition New

--- Projects/Emotion Recognition/main.py
+++ Projects/Emotion Recognition/main.py
@@ -18,17 +18,18 @@ while True:
         # Try to analyze the face using DeepFace
         try:
             # Perform analysis for emotion, age, gender, and race
-            result = DeepFace.analyze(frame, actions=['age'], enforce_detection=False)
+            actions=['age']
+            result = DeepFace.analyze(frame, actions=actions, enforce_detection=False)
 
             # Get the analysis results
-            # emotion = result[0]['dominant_emotion']
             age = result[0]['age']
+            # emotion = result[0]['dominant_emotion']
             # gender = result[0]['gender']
             # race = result[0]['dominant_race']
 
             # Display the analysis results on the frame
-            # cv2.putText(frame, f"Emotion: {emotion}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.putText(frame, f"Age: {age}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame, f"Age: {age}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            # cv2.putText(frame, f"Emotion: {emotion}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             # cv2.putText(frame, f"Gender: {gender}", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
             # cv2.putText(frame, f"Race: {race}", (50, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
@@ -38,6 +39,9 @@ while True:
         # Display the frame with analysis results
         cv2.imshow('Emotion Recognition', frame)
 
+        # Check if 'q' is pressed or the window is closed
+        if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('Emotion Recognition', cv2.WND_PROP_VISIBLE) < 1:
+            break
     else:
         print("Error: Unable to capture frame.")
         break
